70d5e1ff0444c999a4c41b3112cdc11faf843da1
hwanjun <hjun0505@dgist.ac.kr>
2017-11-09 00:11:53 +0900

    revise

--- dgfoodie/views.py
+++ dgfoodie/views.py
@@ -9,40 +9,6 @@ def keyboard(request): ##키보드 정의해주기
         'buttons' : ['학생식당-가마(3500원)', '학생식당-인터쉐프(5000원)','교직원(5000원)']
         })
 
-##
-def get_menu(cafeteria_name):
-    if cafeteria_name == '학생식당-가마(3500원)':
-        lunch = lunch_menu_A[today_num]
-        dinner = dinner_menu_A[today_num]
-        return "--------------\n"+ "<점심>\n" + lunch + "\n \n<저녁>\n" + dinner
-    elif cafeteria_name == '학생식당-인터쉐프(5000원)':
-        lunch = lunch_menu_B[today_num]
-        dinner = dinner_menu_B[today_num]
-        return "--------------\n"+ "<점심>\n" + lunch + "\n \n<저녁>\n" + dinner
-    elif cafeteria_name == '교직원(5000원)':
-        lunch = lunch_menu_C[today_num]
-        dinner = dinner_menu_C[today_num]
-        return "--------------\n"+ "<점심>\n" + lunch + "\n \n<저녁>\n" + dinner
-
-@csrf_exempt  ##post방식 사용에 따른 CSRF Token 에러 제거
-##keyboard에 대한 응답 message
-def message(request):
-    message = ((request.body).decode('utf-8'))
-    return_json_str = json.loads(message)
-    cafeteria_name = return_json_str['content']
-    today_date = datetime.date.today().strftime("%m월 %d일")
-
-    return JsonResponse({
-        'message': {
-            'text': today_date + '의 ' + cafeteria_name + " 메뉴입니다.\n" + get_menu(cafeteria_name)
-        },
-        'keyboard': {
-            'type': 'buttons',
-            'buttons': ['학생식당-가마(3500원)', '학생식당-인터쉐프(5000원)', '교직원(5000원)']
-        }
-    })
-
-
 ##월요일:1 화요일:2 ... 일요일:7
 today_num = datetime.date.today().isocalendar()[2]
 
@@ -95,6 +61,36 @@ dinner_menu_C = {
 5 : '순두부찌개\n야채고로케/케찹\n건파래볶음\n무우생채\n배추김치\n쌀밥',
 }
 
+def get_menu(cafeteria_name): ##각 메뉴의 점심과 저녁 메세지 표현
+    if cafeteria_name == '학생식당-가마(3500원)':
+        lunch = lunch_menu_A[today_num]
+        dinner = dinner_menu_A[today_num]
+        return "--------------\n"+ "<점심>\n" + lunch + "\n \n<저녁>\n" + dinner
+    elif cafeteria_name == '학생식당-인터쉐프(5000원)':
+        lunch = lunch_menu_B[today_num]
+        dinner = dinner_menu_B[today_num]
+        return "--------------\n"+ "<점심>\n" + lunch + "\n \n<저녁>\n" + dinner
+    elif cafeteria_name == '교직원(5000원)':
+        lunch = lunch_menu_C[today_num]
+        dinner = dinner_menu_C[today_num]
+        return "--------------\n"+ "<점심>\n" + lunch + "\n \n<저녁>\n" + dinner
+
+@csrf_exempt  ##post방식 사용에 따른 CSRF Token 에러 제거
+def message(request): ##keyboard에 대한 응답 message
+    message = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(message)
+    cafeteria_name = return_json_str['content']
+    today_date = datetime.date.today().strftime("%m월 %d일")
+
+    return JsonResponse({
+        'message': {
+            'text': today_date + '의 ' + cafeteria_name + " 메뉴입니다.\n" + get_menu(cafeteria_name)
+        },
+        'keyboard': {
+            'type': 'buttons',
+            'buttons': ['학생식당-가마(3500원)', '학생식당-인터쉐프(5000원)', '교직원(5000원)']
+        }
+    })
 
 
 
